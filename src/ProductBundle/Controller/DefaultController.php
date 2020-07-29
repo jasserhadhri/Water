5044ced3f49d43f47a48a90627571861d3ddd4b4
Jasser Hadhri <jasser.hadhri@esprit.tn>
2020-07-29 21:22:57 +0200

    ajout catégorie et marque

--- src/ProductBundle/Controller/DefaultController.php
+++ src/ProductBundle/Controller/DefaultController.php
@@ -2,12 +2,41 @@
 
 namespace ProductBundle\Controller;
 
+use ProductBundle\Entity\Categorie;
+use ProductBundle\Entity\Marque;
+use ProductBundle\Form\CategorieType;
+use ProductBundle\Form\MarqueType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('@Product\Default\index.html.twig');
+    }
+    public function AjouterCategorieAction(Request $request){
+        $categorie = new Categorie();
+        $form =$this->createForm(CategorieType::class,$categorie);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($categorie);
+            $em->flush();
+
+        }
+        return $this->render('@Product\Default\AjouterCategorie.html.twig',array("form"=>$form->createView()));
+    }
+    public function AjouterMarqueAction(Request $request){
+        $marque = new Marque();
+        $form =$this->createForm(MarqueType::class,$marque);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($marque);
+            $em->flush();
+
+        }
+        return $this->render('@Product\Default\AjouterMarque.html.twig',array("form"=>$form->createView()));
     }
 }
---
+++ src/ProductBundle/Form/CategorieType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ProductBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategorieType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre',TextType::class)
+            ->add('description',TextareaType::class)
+            ->add('Enregistrer',SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ProductBundle\Entity\Categorie'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'productbundle_categorie';
+    }
+
+
+}
